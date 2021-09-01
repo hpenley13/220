a2256157095e33c64a33f1b09d745a0c7c83260d
hpenley13 <penleyhl@g.cofc.edu>
2021-09-01 13:29:49 -0400

    Completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,17 @@
+"""
+Name: Harrison Penley
+hello.py
+
+Problem: print hello, world! to the screen
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work
+
+"""
+
+
+def main():
+    print("hello, world!")
+
+
+main()
